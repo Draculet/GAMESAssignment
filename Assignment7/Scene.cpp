550fbc2e25107523fb47c2a315e2aa844592f418
root <root@LAPTOP-5EOOO18A.localdomain>
2022-07-10 21:38:15 +0800

    feat: fin assigment8

--- Assignment7/Scene.cpp
+++ Assignment7/Scene.cpp
@@ -61,4 +61,47 @@ bool Scene::trace(
 Vector3f Scene::castRay(const Ray &ray, int depth) const
 {
     // TO DO Implement Path Tracing Algorithm here
+    auto inter = intersect(ray);
+    if (!inter.happened) return Vector3f();
+    if (inter.m->hasEmission()) return inter.m->getEmission();
+
+    Intersection lightInter;
+    float lightPdf = 0.0f;
+    sampleLight(lightInter, lightPdf);
+    Vector3f p2light = lightInter.coords - inter.coords;
+    Vector3f p2lightDir = p2light.normalized();
+    float p2lightPow = p2light.norm() * p2light.norm();
+
+    Vector3f l_dir; // 直接光照
+
+    Ray p2lightRay(inter.coords, p2lightDir);
+    Intersection t = intersect(p2lightRay);
+    // 由于浮点精度问题可能判断为遮挡,导致光线较暗
+    if (t.distance > p2light.norm() - 0.0001)
+    {
+        // Li * fr * (cosΘ - cosΘ') / p2l_disatance^2
+        l_dir = lightInter.emit * inter.m->eval(ray.direction, p2lightDir, inter.normal) 
+            * dotProduct(p2lightDir, inter.normal) 
+            * dotProduct(-p2lightDir, lightInter.normal) 
+            / p2lightPow / lightPdf;
+    }
+
+    // 轮盘赌判断失败,不继续trace
+    if (get_random_float() > RussianRoulette) return l_dir;
+
+    Vector3f l_indir; // 间接光照
+    // 抽样产生反射ray,交点为q
+    Vector3f p2qDir = inter.m->sample(ray.direction, inter.normal).normalized();
+    Ray p2qRay(inter.coords, p2qDir);
+    Intersection p2qInter = intersect(p2qRay);
+    if (p2qInter.happened && !p2qInter.m->hasEmission())
+    {
+        float pdf = inter.m->pdf(ray.direction, p2qDir, inter.normal);
+        l_indir = castRay(p2qRay, depth + 1) 
+            * inter.m->eval(ray.direction, p2qDir, inter.normal) 
+            * dotProduct(p2qDir, inter.normal)
+            / pdf / RussianRoulette; // 0.8的概率轮盘赌成功
+    }
+
+    return l_dir + l_indir;
 }
